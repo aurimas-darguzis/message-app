610eb1da91084ba5966e0c89fbb62e54ad1d06d5
Aurimas <aurimas@darguzis.com>
2017-02-16 23:27:24 +0000

    Update message.service.ts

--- messages/message.service.ts
+++ messages/message.service.ts
@@ -1,14 +1,37 @@
-import { Message } from './message.model'
+import { Observable } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Message } from './message.model';
+import 'rxjs/Rx';
 
+
+@Injectable()
 export class MessageService {
     messages: Message[] = [];
 
+    constructor(private http: Http) {}
+
     addMessage(message: Message) {
         this.messages.push(message);
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/message', body, { headers: headers })
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
     getMessages() {
-        return this.messages;
+        return this.http.get('http://localhost:3000/message')
+            .map((response: Response) => {
+                const messages = response.json().obj;
+                let transformedMessages: Message[] = [];
+                for (let message of messages) {
+                    transformedMessages.push(new Message(message.content, 'username', message.id, null));
+                }
+                this.messages = transformedMessages;
+                return transformedMessages;
+            })
+            .catch((error: Response) => Observable.throw(error.json()));;
     }
 
     deleteMessage(message: Message) {
