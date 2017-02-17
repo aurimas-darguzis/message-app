5ae4517bb5c4e05bc8f6c5ef60221f12f7c045b4
Aurimas <aurimas@darguzis.com>
2017-02-17 18:42:11 +0000

    refactored to auth module

---
+++ auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { authRouting } from './auth.routing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LogoutComponent } from './logout.component';
+import { SignupComponent } from './signup.component';
+import { SigninComponent } from './signing.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+        SigninComponent,
+        SignupComponent,
+        LogoutComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        authRouting
+    ]
+})
+export class AuthModule {}
--- auth/auth.service.ts
+++ auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { ErrorService } from '../errors/error.service';
 import { Headers, Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { User } from './user.model';
@@ -7,7 +8,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthService {
 
-    constructor(private http: Http) {}
+    constructor(private http: Http, private errorService: ErrorService) {}
 
     signup(user: User) {
         const body = JSON.stringify(user);
@@ -15,7 +16,10 @@ export class AuthService {
         return this.http.post('http://localhost:3000/user', body, {headers: headers})
             // .map is for transforming the data we can back from the server
             .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));;
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json())
+            });
     }
 
     signin(user: User) {
@@ -24,7 +28,10 @@ export class AuthService {
         return this.http.post('http://localhost:3000/user/signin', body, {headers: headers})
             // .map is for transforming the data we can back from the server
             .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));;
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json())
+            });
     }
 
     logout() {
