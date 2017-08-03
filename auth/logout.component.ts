02ecb68ffd05e5e3ccdab448183963ed7d9f65bc
Aurimas <aurimas@darguzis.com>
2017-08-03 08:05:31 +0100

    Update logout.component.ts

--- auth/logout.component.ts
+++ auth/logout.component.ts
@@ -13,7 +13,8 @@ import { Component } from '@angular/core';
 
 export class LogoutComponent {
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService,
+                private router: Router) {}
 
     onLogout() {
         this.authService.logout();
